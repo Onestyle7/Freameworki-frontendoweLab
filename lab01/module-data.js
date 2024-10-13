753a7f89d92316e2765e0a581353e747577ff3aa
Onestyle7 <klusbartek8@gmail.com>
2024-10-13 20:09:02 +0200

    lab01.
    Przerobienie skryptu zwracającego tablicę imion, teraz zwraca tablicę obiektów.

--- lab01/module-data.js
+++ lab01/module-data.js
@@ -1,1 +1,7 @@
-export const data = ["Monika","Piotrek","Kacper","Janek","Robert","Ewa","Kacper","Robert","Stefan","Robert","Monika","Karol","Monika","Robert","Janek","Ola","Ola","Kacper","Ola","Adam","Bartek","Piotrek","Bartek","Monika","Ola","Karol","Piotrek","Piotrek","Piotrek","Adam","Robert","Bartek","Ola","Piotrek","Ewa","Kacper","Ola","Stefan","Janek","Stefan","Monika","Robert","Karol","Bartek","Ola","Kacper","Ola","Janek","Monika","Janek","Ewa","Adam","Karol","Karol","Ewa","Janek","Bartek","Monika","Bartek","Robert","Monika","Stefan","Ola","Piotrek","Stefan","Bartek","Janek","Monika","Ewa","Ewa","Kacper","Monika","Robert","Janek","Ewa","Ola","Stefan","Karol","Karol","Ola","Piotrek","Kacper","Stefan","Monika","Robert","Ewa","Ola","Kacper","Janek","Karol","Piotrek","Kacper","Kacper","Stefan","Janek","Ola","Ewa","Ewa","Monika","Ewa",];
+export const data = [
+    {"id":1,"name":"Karol","birth":"1999-06-10","eyes":"blue"},
+    {"id":2,"name":"Bartek","birth":"1986-01-26","eyes":"grey"},
+    {"id":3,"name":"Kacper","birth":"1970-03-12","eyes":"grey"},
+    {"id":4,"name":"Robert","birth":"1968-07-24","eyes":"grey"},
+    {"id":5,"name":"Adam","birth":"1997-09-15","eyes":"grey"},
+];
