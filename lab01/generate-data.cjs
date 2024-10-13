f0c86930a077715ef06d6f7846367d0836b4c5fa
Onestyle7 <klusbartek8@gmail.com>
2024-10-13 20:09:02 +0200

    lab01.
    Przerobienie skryptu zwracającego tablicę imion, teraz zwraca tablicę obiektów.

--- lab01/generate-data.cjs
+++ lab01/generate-data.cjs
@@ -11,12 +11,20 @@ fs.readFile('./names.txt', 'utf8', (err, data) => {
     //podział łańcucha z imionami na wiersze.
     names = data.split("\n").map(s => s.trim()).filter(n => n.length != 0);
     console.log(names);
+
     let content = "export const data = [";
     for(let i = 0; i < count; i++){
-        //losowanie imienia z bilioteki imion
-        content += `"${names[~~((Math.random() * names.length) / 1)]}",`; 
+        const randomName = names[Math.floor(Math.random() * names.length)];
+        const person = {
+            id: i + 1,
+            name: randomName,
+            birth: randomDate(new Date(1950, 0, 1), new Date(2010, 0, 1)), // Losowa data urodzenia
+            eyes: randomEyeColor()  // Losowy kolor oczu
+        };
+        content += JSON.stringify(person) + ",\n";
     }
     content += "];"
+
     //zapis łańcucha do pliku 
     fs.writeFile('module-data.js', content, (err) => {
         if (err) {
@@ -24,4 +32,18 @@ fs.readFile('./names.txt', 'utf8', (err, data) => {
         }
         console.log("module-data.js generated");
        });
+
+// Funkcja losująca datę w przedziale <min, max>
+
+function randomDate(min, max){
+    const date = new Date(min.getTime() + Math.random() * (max.getTime() - min.getTime()));
+    return date.toISOString().split('T')[0]; // Zwracamy datę w formacie 'YYYY-MM-DD'
+}
+// Funkcja losująca kolor oczu
+
+function randomEyeColor(){
+    const colors = ['blue', 'green', 'brown', 'grey'];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
 });
